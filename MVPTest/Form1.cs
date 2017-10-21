d39dc7f20ebd028517088d14bb007597dc21368e
derosea7 <derosea7@gmail.com>
2017-10-20 20:42:19 -0700

    first commit

---
+++ MVPTest/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVPTest
+{
+  public partial class Form1 : Form, IMyView
+  {
+    MyPresenter Presenter;
+    public Form1()
+    {
+      InitializeComponent();
+      Presenter = new MyPresenter(this);
+    }
+
+    public string SomeData
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+      set
+      {
+        label1.Text = value;
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // Run.
+      Presenter.ChangeData();
+    }
+
+  }
+}
---
+++ MVPTest/MyPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPTest
+{
+  class MyPresenter
+  {
+    private IMyView View { get; set; }
+    public MyPresenter(IMyView view)
+    {
+      View = view;
+      View.SomeData = "test string";
+    }
+
+    public void ChangeData()
+    {
+      View.SomeData = "Some changed data";
+    }
+  }
+}
